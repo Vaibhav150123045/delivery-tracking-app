3d38f7149fcb93bcd087de733817d54bdca5509c
Sameer Kotwal <964sam@gmail.com>
2021-09-15 00:02:07 +0530

    fix routes

--- pages/login.js
+++ pages/login.js
@@ -1,62 +1,93 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { userLoginDetails, userRoles } from './constants';
-import Customer from './customer-journey/customer';
-import Seller from './seller-journey/seller';
-import HubIncharge from './hub-incharge-journey/hubIncharge';
-
-
+import React from "react";
+import { useForm } from "react-hook-form";
+import { userLoginDetails, userRoles } from "./constants";
+import Customer from "./customer-journey/customer";
+import Seller from "./seller-journey/seller";
+import HubIncharge from "./hub-incharge-journey/hubIncharge";
+import { useRouter } from "next/router";
 
 export default function Login() {
   const {
     register,
     handleSubmit,
-    formState: { errors }
+    formState: { errors },
   } = useForm();
+
+  const router = useRouter();
 
   const routeToCorrectJourney = (userRole, userID) => {
     switch (userRole) {
       case userRoles.CR:
-        return (<Customer />);
+        // return <Customer />;
+        router.push("/customer-journey/customer");
+        break;
       case userRoles.HI:
-        return <HubIncharge />
+        // return <HubIncharge />;
+        router.push("/hub-incharge-journey/hubIncharge");
+        break;
+
       case userRoles.SR:
-        return <Seller />
+        // return <Seller />;
+        router.push("/seller-journey/seller");
+        break;
+
       default:
         break;
     }
-  }
+  };
 
   const validateDataAndRetrieveCustomerGroup = (data) => {
     let userRole;
     let userID;
-    userLoginDetails.forEach({} = (item) => {
-      if(item.phoneNumber === data.phoneNumber && item.password === data.password){
+    console.log("userLoginDetails", userLoginDetails);
+    userLoginDetails.forEach((item) => {
+      if (
+        item.phoneNumber === data.phoneNumber &&
+        item.password === data.password
+      ) {
         userRole = item.userRole;
         userID = item.userID;
+        console.log("userRole", userRole);
+        console.log("userID", userID);
         routeToCorrectJourney(userRole, userID);
       }
-    })
-    errors.login = 'Failed';
-  }
+    });
+    errors.login = "Failed";
+  };
 
   const onSubmit = (data) => {
     console.log(data);
     validateDataAndRetrieveCustomerGroup(data);
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div >
-        <input {...register('phoneNumber', { required: true, maxLength: 10, minLength: 10 })} placeholder="Phone Number" />
-          {errors.phoneNumber && errors.phoneNumber.type === "required" && <p>Phone number is required.</p>}
-          {errors.phoneNumber && errors.phoneNumber.type === "maxLength" && <p>Phone nuber should be 10 digit long.</p> }
-          {errors.phoneNumber && errors.phoneNumber.type === "minLength" && <p>Phone nuber should be 10 digit long.</p> }
-        <input type="password" {...register('password', { required: true })} placeholder="Password" />
-          {errors.password && <p>Password is required.</p>}
+      <div>
+        <input
+          {...register("phoneNumber", {
+            required: true,
+            maxLength: 10,
+            minLength: 10,
+          })}
+          placeholder="Phone Number"
+        />
+        {errors.phoneNumber && errors.phoneNumber.type === "required" && (
+          <p>Phone number is required.</p>
+        )}
+        {errors.phoneNumber && errors.phoneNumber.type === "maxLength" && (
+          <p>Phone nuber should be 10 digit long.</p>
+        )}
+        {errors.phoneNumber && errors.phoneNumber.type === "minLength" && (
+          <p>Phone nuber should be 10 digit long.</p>
+        )}
+        <input
+          type="password"
+          {...register("password", { required: true })}
+          placeholder="Password"
+        />
+        {errors.password && <p>Password is required.</p>}
         <input type="submit" />
       </div>
-
-      </form>
+    </form>
   );
 }
